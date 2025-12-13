29e2be91d9b28ee32c88bc1f0266c14446b6c72f
sys6101 <126212736+sys6101@users.noreply.github.com>
2025-12-13 13:18:14 +0700

    update is_super_chat condition to include FORUM chat type (#225)
    * feat: update is_super_chat condition to include FORUM chat type
    
    * feat: include FORUM chat type in user transmission checks
    
    ---------
    
    Co-authored-by: sys6101 <csvmen@gmail.com>

--- bot/helper/common.py
+++ bot/helper/common.py
@@ -136,7 +136,7 @@ class TaskConfig:
         self.thumb = None
         self.excluded_extensions = []
         self.files_to_proceed = []
-        self.is_super_chat = self.message.chat.type.name in ["SUPERGROUP", "CHANNEL"]
+        self.is_super_chat = self.message.chat.type.name in ["SUPERGROUP", "CHANNEL", "FORUM"]
 
         self.yt_privacy = None
         self.yt_mode = "playlist"
@@ -435,7 +435,7 @@ class TaskConfig:
                         self.hybrid_leech = False
                     else:
                         uploader_id = TgClient.user.me.id
-                        if chat.type.name not in ["SUPERGROUP", "CHANNEL", "GROUP"]:
+                        if chat.type.name not in ["SUPERGROUP", "CHANNEL", "GROUP", "FORUM"]:
                             self.user_transmission = False
                             self.hybrid_leech = False
                         else:
@@ -459,7 +459,7 @@ class TaskConfig:
                             raise ValueError("Chat not found!")
                     else:
                         uploader_id = self.client.me.id
-                        if chat.type.name in ["SUPERGROUP", "CHANNEL", "GROUP"]:
+                        if chat.type.name in ["SUPERGROUP", "CHANNEL", "GROUP", "FORUM"]:
                             member = await chat.get_member(uploader_id)
                             if (
                                 not member.privileges.can_manage_chat
